b88f36dc8dc9d6008976c8072d75a5990354aa7f
Chris Guzman <guzmantchris@gmail.com>
2017-01-19 12:19:46 -0500

    WIP add stream

---
+++ src/main/java/com/nexmo/client/voice/endpoints/StreamsEndpoint.java
@@ -0,0 +1,55 @@
+package com.nexmo.client.voice.endpoints;/*
+ * Copyright (c) 2011-2016 Nexmo Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import com.nexmo.client.HttpWrapper;
+import com.nexmo.client.NexmoClientException;
+import com.nexmo.client.voice.Call;
+import com.nexmo.client.voice.CallEvent;
+import com.nexmo.client.voice.CallRecordPage;
+import com.nexmo.client.voice.CallsFilter;
+
+import java.io.IOException;
+
+public class StreamsEndpoint {
+    private final StartStreamMethod startStream;
+    private final StopStreamMethod stopStream;
+
+    public StreamsEndpoint(HttpWrapper httpWrapper) {
+        this.createCall = new CreateCallMethod(httpWrapper);
+        this.readCall = new ReadCallMethod(httpWrapper);
+        this.listCalls = new ListCallsMethod(httpWrapper);
+    }
+
+    public CallEvent post(Call callRequest) throws IOException, NexmoClientException {
+        return this.createCall.execute(callRequest);
+    }
+
+    public CallRecordPage get(CallsFilter filter) throws IOException, NexmoClientException {
+        return this.listCalls.execute(filter);
+    }
+
+    public Call get(String uuid) throws IOException, NexmoClientException {
+        return this.readCall.execute(uuid);
+    }
+
+//    public void put(String uuid) {}
+}
