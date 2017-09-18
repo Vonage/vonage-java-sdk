6c556d79e46950ac0a8cbb5f8c1de0078129180b
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-09-18 10:10:18 +0100

    Verify control (#115)
    * Internal implementation.
    
    * Public interface.
    
    * Update CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,6 +15,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 - Add `NumbersClient.searchNumbers` method.
 - Add `NumbersClient.cancelNumber` method.
 - Add `NumbersClient.buyNumber` method.
+- Add `VerifyClient.advanceVerification` and `VerifyClient.cancelVerification`
 
 ## [2.0.2] - 2017-05-04
 ### Fixed
---
+++ src/main/java/com/nexmo/client/verify/ControlResponse.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2011-2017 Nexmo Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.nexmo.client.verify;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nexmo.client.NexmoUnexpectedException;
+
+import java.io.IOException;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ControlResponse {
+    private final String status;
+    private final VerifyControlCommand command;
+    private String errorText;
+
+    @JsonCreator
+    public ControlResponse(
+            @JsonProperty("status") String status,
+            @JsonProperty("command") VerifyControlCommand command) {
+        this.status = status;
+        this.command = command;
+        this.errorText = null;
+    }
+
+    @JsonProperty
+    public String getStatus() {
+        return status;
+    }
+
+    @JsonProperty
+    public VerifyControlCommand getCommand() {
+        return command;
+    }
+
+    @JsonProperty("error_text")
+    public String getErrorText(){
+        return this.errorText;
+    }
+
+    public static ControlResponse fromJson(String json) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(json, ControlResponse.class);
+        } catch (IOException jpe) {
+            throw new NexmoUnexpectedException("Failed to produce ControlResponse from json.", jpe);
+        }
+    }
+}
