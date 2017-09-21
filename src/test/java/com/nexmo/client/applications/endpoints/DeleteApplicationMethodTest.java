d8a11a8a54ee3fe3c487a9677038384a01e0fa76
Mark Smith <mark.smith@practicalpoetry.co.uk>
2017-09-21 12:55:17 +0100

    Application endpoints - create, update and delete.

---
+++ src/test/java/com/nexmo/client/applications/endpoints/DeleteApplicationMethodTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2011-2017 Nexmo Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.nexmo.client.applications.endpoints;
+
+import com.nexmo.client.TestUtils;
+import com.nexmo.client.auth.TokenAuthMethod;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.RequestBuilder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Map;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+public class DeleteApplicationMethodTest {
+    private DeleteApplicationMethod endpoint;
+
+    @Before
+    public void setUp() throws Exception {
+        this.endpoint = new DeleteApplicationMethod(null);
+    }
+
+    @Test
+    public void testGetAcceptableAuthMethods() throws Exception {
+        Class[] auths = this.endpoint.getAcceptableAuthMethods();
+        assertArrayEquals(new Class[]{TokenAuthMethod.class}, auths);
+    }
+
+    @Test
+    public void testMakeRequest() throws Exception {
+        RequestBuilder builder = this.endpoint.makeRequest("dummy-application-uuid");
+        assertEquals("DELETE", builder.getMethod());
+        assertEquals("https://api.nexmo.com/v1/applications/dummy-application-uuid", builder.build().getURI().toString());
+        
+        Map<String, String> params = TestUtils.makeParameterMap(builder.getParameters());
+        assertEquals(0, params.size());
+    }
+
+    @Test
+    public void testParseResponse() throws Exception {
+        HttpResponse stub = TestUtils.makeJsonHttpResponse(204, "");
+        this.endpoint.parseResponse(stub);
+    }
+}
