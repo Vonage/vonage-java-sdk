bdceb8fe996a27639e98ee158f6998443ac28f9b
Steve Crow <steve.crow@vonage.com>
2018-11-13 13:04:30 -0500

    No need to keep XML behavior.

--- src/main/java/com/nexmo/client/sms/SmsSubmissionResponseMessage.java
+++ src/main/java/com/nexmo/client/sms/SmsSubmissionResponseMessage.java
@@ -21,10 +21,8 @@
  */
 package com.nexmo.client.sms;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
 
 import java.math.BigDecimal;
@@ -40,75 +38,49 @@ public class SmsSubmissionResponseMessage {
     private String errorText;
     private String clientRef;
 
-    @JsonCreator
-    SmsSubmissionResponseMessage(@JsonProperty("to") String to,
-                                 @JsonProperty("message-id") String id,
-                                 @JsonProperty(value = "status", required = true) MessageStatus status,
-                                 @JsonProperty("remaining-balance") String remainingBalance,
-                                 @JsonProperty("message-price") String messagePrice,
-                                 @JsonProperty("network") String network,
-                                 @JsonProperty("error-text") String errorText,
-                                 @JsonProperty("client-ref") String clientRef) {
-
-        // Kind of hacky, but the XML response used null instead of empty strings.
-        this.to = (StringUtils.isNotBlank(to)) ? to : null;
-        this.id = (StringUtils.isNotBlank(id)) ? id : null;
-        this.status = status;
-
-        // Again, this is just to emulate how the XML response was working
-        try {
-            this.remainingBalance = (remainingBalance != null) ? new BigDecimal(remainingBalance) : null;
-        } catch (NumberFormatException nfe) {
-            this.remainingBalance = null;
-        }
-
-        try {
-            this.messagePrice = (messagePrice != null) ? new BigDecimal(messagePrice) : null;
-        } catch (NumberFormatException nfe) {
-            this.messagePrice = null;
-        }
-
-        this.network = (StringUtils.isNotBlank(network)) ? network : null;
-        this.clientRef = (StringUtils.isNotBlank(clientRef)) ? clientRef : null;
-        this.errorText = (StringUtils.isNotBlank(errorText)) ? errorText : null;
-    }
-
+    @JsonProperty("to")
     public String getTo() {
         return this.to;
     }
 
+    @JsonProperty("message-id")
     public String getId() {
         return this.id;
     }
 
+    @JsonProperty("status")
     public MessageStatus getStatus() {
         return this.status;
     }
 
+    @JsonProperty("error-text")
     public String getErrorText() {
         return this.errorText;
     }
 
+    @JsonProperty("client-ref")
     public String getClientRef() {
         return this.clientRef;
     }
 
+    @JsonProperty("remaining-balance")
     public BigDecimal getRemainingBalance() {
         return this.remainingBalance;
     }
 
+    @JsonProperty("message-price")
     public BigDecimal getMessagePrice() {
         return this.messagePrice;
     }
 
+    @JsonProperty("network")
     public String getNetwork() {
         return this.network;
     }
 
     public boolean isTemporaryError() {
-        return this.status == MessageStatus.STATUS_INTERNAL_ERROR
-                || this.status == MessageStatus.STATUS_TOO_MANY_BINDS
-                || this.status == MessageStatus.STATUS_THROTTLED;
+        return this.status == MessageStatus.INTERNAL_ERROR || this.status == MessageStatus.TOO_MANY_BINDS
+                || this.status == MessageStatus.THROTTLED;
     }
 
     @Override
