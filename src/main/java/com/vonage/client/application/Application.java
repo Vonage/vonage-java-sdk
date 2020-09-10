d6ea4f7198c8a100060ca3f20875d849de8401ce
slorello89 <slorello89@gmail.com>
2020-09-10 16:57:36 -0400

    fixing nexmo namespace for capabilities import

--- src/main/java/com/vonage/client/application/Application.java
+++ src/main/java/com/vonage/client/application/Application.java
@@ -27,7 +27,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.vonage.client.VonageUnexpectedException;
-import com.nexmo.client.application.capabilities.*;
+import com.vonage.client.application.capabilities.*;
 import com.vonage.client.application.capabilities.*;
 
 import java.io.IOException;
