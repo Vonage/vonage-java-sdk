70f8a3f48b78f1e245a90c6796d041d4b9ab2f6c
Shubham Sood <41718296+ShubhamSood1406@users.noreply.github.com>
2020-10-02 13:04:16 +0530

    remove @author comments

--- src/main/java/com/vonage/client/sms/callback/AbstractMOServlet.java
+++ src/main/java/com/vonage/client/sms/callback/AbstractMOServlet.java
@@ -43,7 +43,6 @@ import java.util.concurrent.Executors;
  * the acknowledgement to a minimum in order to maintain throughput when operating at any sort of volume.
  * You are responsible for persisting this object in the event of any failure whilst processing
  *
- * 
  */
 public abstract class AbstractMOServlet extends HttpServlet {
 
