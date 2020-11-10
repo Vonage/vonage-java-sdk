4a94bbd2384db2f5bb60233db3e37737c279d343
Yvonne Allen <princess0115@gmail.com>
2020-11-10 18:04:56 -0500

    Bump version: 5.5.0 → 5.6.0

--- src/main/java/com/vonage/client/HttpWrapper.java
+++ src/main/java/com/vonage/client/HttpWrapper.java
@@ -31,7 +31,7 @@ import java.nio.charset.Charset;
  */
 public class HttpWrapper {
     private static final String CLIENT_NAME = "vonage-java-sdk";
-    private static final String CLIENT_VERSION = "5.5.0";
+    private static final String CLIENT_VERSION = "5.6.0";
     private static final String JAVA_VERSION = System.getProperty("java.version");
 
     private AuthCollection authCollection;
