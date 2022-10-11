95ee008dcd2c09e861a7edba3864d7b8eb55ec0f
Sina Madani <sina.madani@vonage.com>
2022-10-11 11:12:43 +0100

    Implement Moderation endpoints (#414)

---
+++ src/test/java/com/vonage/client/video/MuteSessionRequestTest.java
@@ -0,0 +1,64 @@
+/*
+ *   Copyright 2022 Vonage
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package com.vonage.client.video;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class MuteSessionRequestTest {
+	
+	@Test
+	public void testSerialize() {
+		List<String> excludedStreamIds = Arrays.asList("ID_0", "ID_1", "ID_2");
+		MuteSessionRequest request = new MuteSessionRequest(true, excludedStreamIds);
+		String expectedJson = "{\"active\":true,\"excludedStreamIds\":[\"ID_0\",\"ID_1\",\"ID_2\"]}";
+		assertEquals(expectedJson, request.toJson());
+	}
+
+	@Test
+	public void testSerializeEmptyList() {
+		MuteSessionRequest request = new MuteSessionRequest(false, Collections.emptyList());
+		String expectedJson = "{\"active\":false,\"excludedStreamIds\":[]}";
+		assertEquals(expectedJson, request.toJson());
+	}
+
+	@Test
+	public void testSerializeEmptyVarargs() {
+		MuteSessionRequest request = new MuteSessionRequest(false);
+		String expectedJson = "{\"active\":false,\"excludedStreamIds\":[]}";
+		assertEquals(expectedJson, request.toJson());
+	}
+
+	@Test
+	public void testSerializeEmptyArray() {
+		String[] streamIds = {};
+		MuteSessionRequest request = new MuteSessionRequest(true, streamIds);
+		String expectedJson = "{\"active\":true,\"excludedStreamIds\":[]}";
+		assertEquals(expectedJson, request.toJson());
+	}
+
+	@Test
+	public void testSerializeNullIds() {
+		List<String> streamIds = null;
+		MuteSessionRequest request = new MuteSessionRequest(false, streamIds);
+		String expectedJson = "{\"active\":false}";
+		assertEquals(expectedJson, request.toJson());
+	}
+
+}
