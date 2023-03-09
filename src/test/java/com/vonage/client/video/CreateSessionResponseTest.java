0374d2edca83080118783058af8c92453fa15968
Sina Madani <sina.madani@vonage.com>
2023-03-09 16:55:32 +0000

    Updated copyright years

--- src/test/java/com/vonage/client/video/CreateSessionResponseTest.java
+++ src/test/java/com/vonage/client/video/CreateSessionResponseTest.java
@@ -1,4 +1,4 @@
-/* Copyright 2022 Vonage
+/* Copyright 2023 Vonage
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
