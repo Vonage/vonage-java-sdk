9d91517b4dfc95418ea08565fde475877d26366a
Sina Madani <sina.madani@vonage.com>
2024-04-08 17:41:45 +0100

    Set default annotations on ObjectMapper

--- src/main/java/com/vonage/client/Jsonable.java
+++ src/main/java/com/vonage/client/Jsonable.java
@@ -15,7 +15,9 @@
  */
 package com.vonage.client;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
@@ -38,6 +40,8 @@ public interface Jsonable {
 	static ObjectMapper createDefaultObjectMapper() {
 		return new ObjectMapper()
 				.registerModule(new JavaTimeModule())
+				.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+				.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
 				.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 	}
 
