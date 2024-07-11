9f15f5ddaeda5f1383b7690a83ccafba0cd715a5
Sina Madani <sina.madani@vonage.com>
2024-07-11 14:49:35 +0100

    Save private key in JWTAuthMethod

--- bumpversion.sh
+++ bumpversion.sh
@@ -6,6 +6,6 @@ fi
 
 mvn versions:set -DnewVersion=$1
 mvn validate
-mvn versions:display-plugin-updates
-mvn versions:display-dependency-updates
-rm pom.xml.releaseBackup pom.xml.versionsBackup
+rm pom.xml.versionsBackup #pom.xml.releaseBackup
+#mvn versions:display-plugin-updates
+#mvn versions:display-dependency-updates
