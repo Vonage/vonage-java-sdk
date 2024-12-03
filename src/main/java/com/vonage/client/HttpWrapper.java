09447d708d2a9a09416e296707330da2726ef4f6
Sina Madani <sina.madani@vonage.com>
2024-12-03 12:42:30 +0000

    Bump version

--- src/main/java/com/vonage/client/HttpWrapper.java
+++ src/main/java/com/vonage/client/HttpWrapper.java
@@ -37,7 +37,7 @@ import java.util.UUID;
 public class HttpWrapper {
     private static final String
             CLIENT_NAME = "vonage-java-sdk",
-            CLIENT_VERSION = "8.14.0",
+            CLIENT_VERSION = "8.15.0",
             JAVA_VERSION = System.getProperty("java.version"),
             USER_AGENT = String.format("%s/%s java/%s", CLIENT_NAME, CLIENT_VERSION, JAVA_VERSION);
 
